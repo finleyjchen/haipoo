3dac70cf9cacd47c24138b8fcaa94ab160552029
Finley <finleyjchen@gmail.com>
2020-04-10 14:14:46 -0700

    added favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,7 @@ module.exports = {
         background_color: `#f7f0eb`,
         theme_color: `#a2466c`,
         display: `standalone`,
+        icon: `src/images/favicon.svg`,
       },
     },
     "gatsby-plugin-offline",
